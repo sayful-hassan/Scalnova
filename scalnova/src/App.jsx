7b158c6466dfe3de51ff0abb6b369832add0db77
sayful-hassan <sayf.fashion@gmail.com>
2025-03-06 10:26:23 +0530

    Created hero component

--- scalnova/src/App.jsx
+++ scalnova/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Hero from './components/Hero/Hero'
 import NavBar from './components/Navbar/NavBar'
 
 function App() {
   return (
     <div className="app">
       <NavBar/>
+      <Hero />
     </div>
   )
 }
---
+++ scalnova/src/components/Hero/Hero.jsx
@@ -0,0 +1,8 @@
+import './Hero.css'
+export default function Hero() {
+    return (
+      <section className="hero">
+        <h1>Empowering Businesses with Stunning Websites & Scalable Solutions</h1>
+      </section>
+    )
+  }
