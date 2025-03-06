152ca0b90a2831fbb6d9d916c92d21808ccd4e67
sayful-hassan <sayf.fashion@gmail.com>
2025-03-06 10:30:28 +0530

    Created What we do component

--- scalnova/src/App.jsx
+++ scalnova/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Hero from './components/Hero/Hero'
 import NavBar from './components/Navbar/NavBar'
+import WhatWeDo from './components/WhatWeDo/WhatWeDo'
 
 function App() {
   return (
     <div className="app">
       <NavBar/>
       <Hero />
+      <WhatWeDo />
     </div>
   )
 }
---
+++ scalnova/src/components/WhatWeDo/WhatWeDo.jsx
@@ -0,0 +1,15 @@
+import './WhatWeDo.css'
+export default function WhatWeDo() {
+    return (
+      <section className="what-we-do">
+        <h2>What We Do</h2>
+        <p>We help small to large businesses establish a strong online presence with high-quality web design and development.</p>
+        
+        <ul>
+          <li>Web Design</li>
+          <li>Web Development</li>
+          <li>UI/UX Design</li>
+        </ul>
+      </section>
+    )
+  }
